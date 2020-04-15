898c88bf8413363633694325a76653ebbc240963
Yuanxiang Ying <yingyuanxiang34@sina.com>
2020-04-15 17:52:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
-import numpy
 
 __version__ = "can't find version.py"
 exec(compile(open('version.py').read(),
              'version.py', 'exec'))
 
 install_requires = [
-    'numpy==1.16.0',
-    'pandas==0.23.4'
+    'numpy==1.18.1',
+    'pandas==0.25.3'
 ]
 
 setup(
@@ -16,7 +15,6 @@ setup(
     packages=find_packages(),
     version=__version__,
     install_requires=install_requires,
-    include_dirs=[numpy.get_include()],
     requires=['numpy', 'pandas'],
     python_requires='>=3.6.0',
     package_data={'': ['*.txt']}
