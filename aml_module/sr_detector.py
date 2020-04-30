628314be33a22ea2fd603fc54e5b4bd72f58cc61
yuyi@microsoft.com <Yuanxiang.Ying@microsoft.com>
2020-04-30 17:12:11 +0800

    add plots

--- aml_module/sr_detector.py
+++ aml_module/sr_detector.py
@@ -1,6 +1,28 @@
 import pandas as pd
 from msanomalydetector import SpectralResidual, DetectMode
+import matplotlib.pyplot as plt
 from azureml.studio.core.logger import module_logger as logger
+from azureml.core.run import Run
+
+
+def log_plot_result(input_df, output_df, col_name, mode):
+    plt.figure(figsize=(20, 10))
+    ax1 = plt.subplot(211)
+    if mode == 'AnomalyAndMargin':
+        ax1.fill_between(output_df.index, output_df['lowerBoundary'], output_df['upperBoundary'], color='grey', alpha=0.2, zorder=1)
+        ax1.plot(output_df.index, output_df['expectedValue'], alpha=0.5, label='expected value', zorder=8)
+    ax1.plot(input_df.index, input_df['value'], label='value', zorder=5)
+    ax1.legend()
+    anomalies = input_df[output_df['isAnomaly']]
+    ax1.scatter(anomalies.index, anomalies['value'], c='red', zorder=10)
+    ax1.set_title(col_name)
+
+    ax2 = plt.subplot(212)
+    ax2.plot(output_df.index, output_df['mag'])
+    ax2.set_title('mag')
+
+    run = Run.get_context()
+    run.log_image(col_name, plot=plt)
 
 
 def sr_detect(frame, detect_mode, batch_size, threshold, sensitivity):
@@ -23,6 +45,7 @@ def detect(timestamp, data_to_detect, detect_mode, batch_size, threshold=0.3, se
         frame['timestamp'] = timestamp
         frame['value'] = data_to_detect.iloc[:, 0]
         output = sr_detect(frame, detect_mode, batch_size, threshold, sensitivity)
+        log_plot_result(frame, output, data_to_detect.columns[0], detect_mode)
     else:
         logger.debug(f'detect {column_length} columns')
         output = pd.DataFrame()
@@ -34,5 +57,6 @@ def detect(timestamp, data_to_detect, detect_mode, batch_size, threshold=0.3, se
             result = sr_detect(frame, detect_mode, batch_size, threshold, sensitivity)
             result.columns = [f'{rc}_{col}' for rc in result.columns]
             output = pd.concat((output, result), axis=1)
+            log_plot_result(frame, output, col, detect_mode)
 
     return output
